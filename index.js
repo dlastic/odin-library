fce3699e9ffb84fdd508c6e1c4973aa9737e0b27
dlastic <daniellastic@gmail.com>
2025-04-18 17:54:51 +0200

    Implement displayBooks function to render books in the library

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read, id);
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  const bookList = document.querySelector(".container");
+  bookList.innerHTML = "";  
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+    bookCard.innerHTML = `
+      <h2>${book.title}</h2>
+      <p>${book.author}</p>
+      <p>${book.pages} pages</p>
+      <button>${book.read ? "Read" : "Not Read"}</button>      
+      <button class="remove-button" data-id="${book.id}">Remove</button>
+    `;
+    bookList.appendChild(bookCard);
+  });
+}
